a3734f28c4a341a4266ea5f2c5a4dd6d92073c3b
pavel-sazonov <wismyl@ya.ru>
2019-06-25 18:52:29 +0300

    configure table

--- CitiesWeather/CitiesTableViewController.swift
+++ CitiesWeather/CitiesTableViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class CitiesTableViewController: UITableViewController {
     
-    var cities: City?
+    var cities = [City]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.tableFooterView = UIView()
+        
         navigationItem.title = "Cities"
         
         view.backgroundColor = .white
+        
+        // test cities
+        cities += [
+            City(name: "Moscow", temp: 25),
+            City(name: "Samara", temp: 30),
+            City(name: "Sochi", temp: 32)
+        ]
     }
 
 
 }
 
+// MARK: - Table View Data Source
+
+extension CitiesTableViewController {
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        cell.textLabel?.text = cities[indexPath.row].name
+        
+        return cell
+    }
+}
+
+// MARK: - Table View Delegate
+
+extension CitiesTableViewController {
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
